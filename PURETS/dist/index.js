f9d0e610a9e56e5eddb7c6422404ab3f07f7b4d6
francoariel1492 <francoariel1492@gmail.com>
2023-03-13 16:47:47 -0300

    getters and setters

--- PURETS/dist/index.js
+++ PURETS/dist/index.js
@@ -13,7 +13,25 @@ class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
         this.city = "Cordoba";
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    //getter
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    //setter
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 const franco = new User("franco@parente.com", "Franco");
--- PURETS/src/index.ts
+++ PURETS/src/index.ts
@@ -12,12 +12,32 @@
 //THIS IS THE SAME CODE BUT IN A MORE CLEAR WAY IN MY OPINION
 class User {
 
+  private _courseCount = 1
   readonly city: string = "Cordoba";
   constructor(
     public email:string,
     public name:string,
     // private userId: string
     ){
+  }
+  private deleteToken(){
+    console.log("Token deleted")
+  }
+
+  //getter
+  get getAppleEmail(): string{
+    return `apple${this.email}`
+  }
+
+  get courseCount(): number{
+    return this._courseCount
+  }
+  //setter
+  set courseCount(courseNum){
+    if(courseNum <= 1){
+        throw new Error("Course count should be more than 1")
+    }
+    this._courseCount = courseNum
   }
 }
 
