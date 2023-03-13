93e96dfb8eadf4c22c4736e63018df993e0134e3
francoariel1492 <francoariel1492@gmail.com>
2023-03-13 16:33:51 -0300

    TS classes second part

--- PURETS/dist/index.js
+++ PURETS/dist/index.js
@@ -1,10 +1,20 @@
 "use strict";
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "Cordoba";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+//THIS IS THE SAME CODE BUT IN A MORE CLEAR WAY IN MY OPINION
 class User {
     constructor(email, name) {
-        this.city = "Cordoba";
         this.email = email;
         this.name = name;
+        this.city = "Cordoba";
     }
 }
 const franco = new User("franco@parente.com", "Franco");
-// franco.city = "Buenos Aires"
+// franco.name
--- PURETS/src/index.ts
+++ PURETS/src/index.ts
@@ -1,12 +1,25 @@
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "Cordoba";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+
+//THIS IS THE SAME CODE BUT IN A MORE CLEAR WAY IN MY OPINION
 class User {
-  email: string;
-  name: string;
+
   readonly city: string = "Cordoba";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
+  constructor(
+    public email:string,
+    public name:string,
+    // private userId: string
+    ){
   }
 }
 
 const franco = new User("franco@parente.com", "Franco");
-// franco.city = "Buenos Aires"
+// franco.name
