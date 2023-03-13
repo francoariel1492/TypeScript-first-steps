68914f5af3b6af7e5d85912e7cdb9bbf6d66e5ad
francoariel1492 <francoariel1492@gmail.com>
2023-03-13 15:18:00 -0300

    Interface second part

--- myInterface.ts
+++ myInterface.ts
@@ -8,10 +8,23 @@ interface User {
   getCoupon(couponname: string, value: number): number;
 }
 
-const franco: User = {
+
+interface User{
+  githubToken?: string,
+}
+
+interface Admin extends User {
+  role: "admin" | "ta" | "learner" 
+}
+
+
+
+const franco: Admin = {
   dbId: 22,
   email: "franco@parente.com",
   userId: 2211,
+  role: "admin",
+  githubToken: "github",
   startTrail: () => {
     return "trail started";
   },
