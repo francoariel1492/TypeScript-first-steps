324303e89ca12762df2f698ee37bd2ce5ae8ba0e
francoariel1492 <francoariel1492@gmail.com>
2023-03-16 00:15:13 -0300

    Generics part one

---
+++ PURETS/dist/generics.js
@@ -0,0 +1,19 @@
+"use strict";
+const score = [];
+const names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+// identityThree(true)
+//this one is shortcut way to write the code
+// previously writed,
+function identityFour(val) {
+    return val;
+}
+identityFour({});
